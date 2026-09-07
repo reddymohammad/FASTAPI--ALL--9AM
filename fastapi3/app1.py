2777bac9ace7102401ea6582ab03ebd6cf05dcda
reddymohammad <reddymohammed779@gmail.com>
2026-09-07 10:25:31 +0530

    today

---
+++ fastapi3/app1.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+app=FastAPI()
+
+@app.get("/")
+def home_page():
+  return{'msg':'application root request'}
+@app.post("/")
+def create_user():
+  return{'msg':'new user created'}
+@app.put("/")
+def update_user():
+  return{'msg':'user update successfully'}
+@app.delete("/")
+def delete_user():
+  return{'msg':'deleted successfully'}
